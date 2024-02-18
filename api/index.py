aec4df5357c9706d01cc7731f0a7ca57727e6ffd
smartsiva <52667587+smartsiva@users.noreply.github.com>
2024-02-18 23:08:56 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -51,3 +51,42 @@ def retrieve_mainline_data():
         if results == []:
             return "No Mainline IPO is Currently Open, Please Check Tomorrow!"
         return results
+
+@app.route('/fetch_upcoming')
+def retrieve_upcoming_ipo():
+    url = "https://www.investorgain.com/report/live-ipo-gmp/331/ipo/"
+
+    # Send an HTTP request to the webpage
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        # Parse the HTML content using lxml
+        tree = html.fromstring(response.content)
+
+        # Find the table element
+        table_element = tree.xpath('//table[@id="mainTable"]')[0]
+
+        # Extract rows from the table
+        rows = table_element.xpath('.//tr')
+
+        results = []
+        for row in rows:
+            # Check if the row has the class 'color-green'
+            row_dict = {}
+
+            # Extract columns from the row
+            columns = row.xpath('.//td')
+            for each in columns:
+                data_label = each.get('data-label')
+                if data_label == "IPO":
+                    # Use regular expressions to extract specific information
+                    if "Upcoming" in each.text_content():
+                        row_dict["company_name"] = each.text_content().split("Upcoming")[0]
+
+                        row_dict[data_label] = each.text_content()
+
+                    results.append(row_dict)
+
+        if results == []:
+            return "No Upcoming IPO is Currently Open, Please Check Tomorrow!"
+        return results
